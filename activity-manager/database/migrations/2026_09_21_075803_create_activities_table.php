ded8b9420863f1ae00d24936325be5292fc4d2dc
taniadwip <taniadwip15@gmail.com>
2026-09-21 15:36:24 +0700

    Membuat Seeder

--- activity-manager/database/migrations/2026_09_21_075803_create_activities_table.php
+++ activity-manager/database/migrations/2026_09_21_075803_create_activities_table.php
@@ -10,12 +10,17 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('activities', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('activities', function (Blueprint $table) {
+        $table->id();
+        $table->string('title', 100);
+        $table->text('description')->nullable();
+        $table->date('activity_date');
+        $table->string('category', 50);
+        $table->string('status', 20)->default('Planned');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
